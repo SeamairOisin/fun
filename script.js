0f1891b80a42d44b03b3976e0ab94065e8b42de9
EugeneFelix <ontharros@gmail.com>
2022-04-21 11:41:34 +0530

    commit

--- script.js
+++
@@ -1,7 +0,0 @@
-function changeImage() {
-    var image = document.getElementById('hello');
-    var base = "https://starzone.ragalahari.com/june2017/photosessions/priyanka-augustin-in-half-saree/priyanka-augustin-in-half-saree";
-    var num = 33;
-//    num = num + 1;
-        image.src = base + num + ".jpg";
-}
