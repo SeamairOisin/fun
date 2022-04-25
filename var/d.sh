589ab7f9fe1a73b4e2bc994df75869c8a8bccb3c
EugeneFelix <ontharros@gmail.com>
2022-04-25 08:06:53 +0530

    commit

--- var/d.sh
+++ var/d.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ext=".jpg"
 name="image"
-for position in {39..163}
+for position in {24..158}
 do
-    curl -o "${name}${position}${ext}" "https://starzone.ragalahari.com/may2017/photosessions/sanju-prasanna-in-hd-wallpapers/sanju-prasanna-in-hd-wallpapers${position}${ext}"
+    curl -o "${name}${position}${ext}" "https://starzone.ragalahari.com/feb2016/photosessions/diksha-panth-half-saree-ragalahari/diksha-panth-half-saree-ragalahari${position}${ext}"
     xdg-open "${name}${position}${ext}"
     termux-toast -g top -b '#00CAB1' -c black -s $position
-    sleep 7
+    sleep 13
     rm "${name}${position}${ext}"
 done
