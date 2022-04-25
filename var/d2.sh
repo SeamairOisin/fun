85c5dbd4a36f6a2eb16cfb45fa91f1fd88571994
EugeneFelix <ontharros@gmail.com>
2022-04-25 13:48:07 +0530

    commit

--- var/d2.sh
+++ var/d2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ext=".jpg"
 name="image"
-for position in {50..100}
+for position in {9..22}
 do
     curl -o "${name}${position}${ext}" "https://starzone.ragalahari.com/mar2011/starzone/deeksha-seth-high-resolution-wanted/deeksha-seth-high-resolution-wanted${position}${ext}"
     xdg-open "${name}${position}${ext}"
     termux-toast -g top -b '#000000' -c white -s $position
-    sleep 20
+    sleep 15
     rm "${name}${position}${ext}"
 done
